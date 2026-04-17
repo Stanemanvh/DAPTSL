47dc0ce301fc693d1f6448bfb3032d040c8830e6
Stan van Honk <svhonk@int6.local.snellius.surf.nl>
2026-04-17 10:34:51 +0200

    added linearprobe.sh

--- Linearprobe.sh
+++ Linearprobe.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=finetune30ksamplesfmow
+#SBATCH --job-name=LPfulldatapretrained25epochseval1500
 #SBATCH --output=%x_%j.out    
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --time=01:00:00
+#SBATCH --time=02:30:00
 #SBATCH --partition=gpu_a100
 
 
@@ -22,26 +22,25 @@ base_csv_dir="${base_dir}/fmow_csvs"
 base_experiment_dir="${base_dir}/explora_linprobe"
 
 # ============ Config ============
-# cfg_file="linprobe/configs/fmow_vitb14.yaml"
-cfg_file="linprobe/configs/fmow_vitl14.yaml"
+cfg_file="linprobe/configs/fmow_vitb14.yaml"
+# cfg_file="linprobe/configs/fmow_vitl14.yaml"
 # cfg_file="linprobe/configs/fmow_vitg14.yaml"
+# cfg_file=linprobe/configs/vitb14_pretrain.yaml
 
 # ============ Pretrained weights ============
 # Vanilla DINOv2 pretrained weights. If using this, set the cfg_file to be linprobe/configs/vitl14_pretrain.yaml
-# pretrained_weights="${base_dir}/dinov2_vitl14_pretrain.pth"
+# pretrained_weights="${base_dir}/dinov2_vitb14_pretrain.pth"
 
 # ExPLoRA pretrained weights
-pretrained_weights="${base_dir}/explora_dinov2_vit_large_fmow_rgb_encoder_only.pth"
+pretrained_weights="${base_dir}/teacher_checkpoint.pth"
 
 # ============ Output directory ============
-out_dir="${base_experiment_dir}/lp30konpretrained"
+out_dir="${base_experiment_dir}/fulldatapretrained25epochseval1500"
 hf_train_max_samples=0  # e.g., 30000; 0 disables cap
 
 # ============ Run ============
 export PYTHONPATH=.
 WANDB__SERVICE_WAIT=300 torchrun --nproc_per_node=1 --master_port=40001 -m linprobe.linear \
-    --wandb=explora_linprobe \
-    --wandb_entity=TODO \
     --train-dataset="fmow:data_and_checkpoints/fmow_csvs/train_62classes.csv" \
     --val-dataset="fmow:data_and_checkpoints/fmow_csvs/test_62classes.csv" \
     --config-file="${cfg_file}" \
@@ -49,7 +48,7 @@ WANDB__SERVICE_WAIT=300 torchrun --nproc_per_node=1 --master_port=40001 -m linpr
     --pretrained-weights="${pretrained_weights}" \
     --batch-size=256 \
     --hf-train-max-samples=${hf_train_max_samples} \
-    --epochs=10 \
+    --epochs=25 \
     --epoch-length=1500 \
     --save-checkpoint-frequency=1500 \
     --eval-period-iterations=1500
